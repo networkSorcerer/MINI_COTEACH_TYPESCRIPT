fcd4ad43e3a2b21dba514a6025b56598b29fd068
networdSorcerer <gigattfyhig@gmail.com>
2025-06-12 15:43:11 +0900

    week3 day4

--- src/pages/PlaylistPage/components/EmptyPlaylistWithSearch.tsx
+++ src/pages/PlaylistPage/components/EmptyPlaylistWithSearch.tsx
@@ -1,30 +1,100 @@
-import { TextField, Typography } from '@mui/material';
+import { Box, InputAdornment, styled, TextField, Typography } from '@mui/material';
 import React, { useState } from 'react';
 import useSearchItemsByKeyword from '../../../hooks/useSearchItemsByKeyword';
 import { SEARCH_TYPE } from '../../../models/search';
-import SearchResulList from './SearchResulList';
+import LoadingScreen from '../../../common/components/LoadingScreen';
+import SearchResultList from './SearchResulList';
+import SearchIcon from '@mui/icons-material/Search';
 
+const StyledTextField = styled(TextField)(({ theme }) => ({
+  width: '100%',
+
+  '& .MuiInputBase-root': {
+    borderRadius: '4px', // 입력 필드의 둥근 모서리
+    backgroundColor: theme.palette.action.active, // 입력 필드의 배경 색상
+    color: 'white', // 텍스트 색상
+  },
+  '& .MuiOutlinedInput-root': {
+    '& fieldset': {
+      borderColor: 'transparent', // 테두리 색상 제거
+    },
+    '&:hover fieldset': {
+      borderColor: 'gray', // 마우스 호버 시 테두리 색상
+    },
+    '&.Mui-focused fieldset': {
+      borderColor: 'gray', // 포커스 시 테두리 색상
+    },
+  },
+}));
+const SearchContainer = styled(Box)({
+  // 스크롤 디자인
+  padding: '16px',
+  width: '100%',
+  height: '100%',
+  overflowY: 'auto',
+
+  '&::-webkit-scrollbar': {
+    display: 'none',
+  },
+  msOverflowStyle: 'none', // IE and Edge
+  scrollbarWidth: 'none', // Firefox
+});
 const EmptyPlaylistWithSearch = () => {
   const [keyword, setKeyword] = useState<string>('');
-  const { data, error, isLoading } = useSearchItemsByKeyword({
+  // 무한스크롤을 위해 nextpage 관련 추가
+  const { data, error, isLoading, hasNextPage, isFetchingNextPage, fetchNextPage } = useSearchItemsByKeyword({
     q: keyword,
-    type: [SEARCH_TYPE.Track, SEARCH_TYPE.Album],
+    type: [SEARCH_TYPE.Track],
   });
-  console.log('search', data);
+  // 모든 페이지를 가져오기 위해 flatMap으로 바꿈
+  const tracks = data?.pages.flatMap((page) => page.tracks?.items) ?? []; //널 병합 연산자라고 불립니다. 이 연산자는 왼쪽의 값이 null 또는 undefined일 때 오른쪽의 값을 반환하고, 그렇지 않으면 왼쪽의 값을 반환합니다.
+  const hasResults = tracks.length > 0;
   const handleSearchKeyword = (event: React.ChangeEvent<HTMLInputElement>) => {
     setKeyword(event.target.value);
   };
+
   return (
-    <div>
-      <Typography variant="h1" my="10px">
-        Let's Find Something For Your Playlist
-      </Typography>
-      <TextField value={keyword} onChange={handleSearchKeyword}></TextField>
-      {data?.pages.map((item) => {
-        if (!item.tracks) return false;
-        return <SearchResulList list={item.tracks?.items} />;
-      })}
-    </div>
+    <SearchContainer>
+      <Box display="inline-block">
+        <Typography variant="h1" my="10px">
+          Let's find something for your playlist
+        </Typography>
+
+        <StyledTextField
+          value={keyword}
+          autoComplete="off"
+          variant="outlined"
+          placeholder="Search for songs or episodes"
+          fullWidth
+          slotProps={{
+            input: {
+              startAdornment: (
+                <InputAdornment position="start">
+                  <SearchIcon style={{ color: 'white' }} />
+                </InputAdornment>
+              ),
+            },
+          }}
+          onChange={handleSearchKeyword}
+        />
+      </Box>
+      <div>
+        {isLoading ? (
+          <LoadingScreen /> // 로딩 중일 때 스피너 표시
+        ) : hasResults ? (
+          <SearchResultList // nextpage관련 속성 추가
+            list={tracks}
+            hasNextPage={hasNextPage}
+            isFetchingNextPage={isFetchingNextPage}
+            fetchNextPage={fetchNextPage}
+          />
+        ) : keyword === '' ? (
+          <></> // 검색어가 없을 때는 아무것도 표시하지 않음
+        ) : (
+          <div>{`No Result for "${keyword}"`}</div> // 검색 결과가 없을 때만 표시
+        )}
+      </div>
+    </SearchContainer>
   );
 };
 
